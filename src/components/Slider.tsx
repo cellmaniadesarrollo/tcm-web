e75ebbcf91a82c0fb59a0efbd9e5dac79e50710b
Jacobs-m <jacobom5710034.tcm@gmail.com>
2025-04-14 17:43:23 -0500

    ADD TRADUCTOR

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -684,6 +684,61 @@ const Slider = () => {
 					</div>
 				</div>
 			</SwiperSlide>
+
+			<SwiperSlide>
+				<div className="w-full h-[664px] md:h-80 z-0">
+					<div className="w-full h-full">
+						<img
+							alt="quantum-gradient-background"
+							src="/images/backgrounds/quantum-gradient.svg"
+							className="object-cover h-full"
+							width={864}
+							height={664}
+							loading={"eager"}
+						/>
+					</div>
+					<div className="absolute h-full z-20 top-0 w-full">
+						<section className="grid h-full grid-cols-1 md:grid-cols-3 mx-5 md:ml-20 md:mr-7">
+							<div className="col-span-1 md:col-span-2 md:mt-20 mt-10">
+								<header>
+									<h2 className="font-roboto font-bold text-white text-center md:text-left text-5xl">
+										<span className="block">TIMEKETTLE X1</span>
+										<span>Traductor Interpreter Hub</span>
+									</h2>
+								</header>
+								<section className="mt-6 flex justify-center md:justify-start">
+									<div className="items-center fade-up inline-flex flex-wrap gap-4">
+										<a
+											target="_blank"
+											rel="noopener noreferrer"
+											href={STOREPAGE_URL + "/products/traductor-timekettle-x1-interpreter-hub"}
+											className="btn bg-black text-zinc-300"
+										>
+											<span className="text-xl md:text-base font-bold">
+												Comprar ahora
+											</span>
+										</a>
+									</div>
+								</section>
+							</div>
+							<section className="relative z-20 mt-20">
+								<div className="relative w-full h-52 flex justify-center items-center">
+									<div className="absolute ml-24 md:ml-16 mb-14 md:mb-6 brightness-75 saturate-50 contrast-200">
+										<img
+											src="/images/products/traductor.webp"
+											className="h-80 w-[218px] md:h-56  aspect-auto"
+											width={218}
+											height={320}
+											alt="touchsurface3pro"
+											loading={"eager"}
+										/>
+									</div>
+								</div>
+							</section>
+						</section>
+					</div>
+				</div>
+			</SwiperSlide>
 			<SwiperNavButton />
 		</Swiper>
 	);
